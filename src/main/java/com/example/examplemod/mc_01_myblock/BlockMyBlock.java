6b8b123fda294f332c73b6e80394750729e5eb45
harutiro <harutiro2027@outlook.jp>
2023-05-08 11:02:28 +0900

    InitalCommit

---
+++ src/main/java/com/example/examplemod/mc_01_myblock/BlockMyBlock.java
@@ -0,0 +1,12 @@
+package com.example.examplemod.mc_01_myblock;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+
+public class BlockMyBlock extends Block {
+    public BlockMyBlock() {
+        super(BlockBehaviour.Properties.of(Material.DIRT));
+    }
+
+}
