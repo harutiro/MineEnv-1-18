27d572de46072d9cbfc866d3c38e0df7f5bf946f
harutiro <harutiro2027@outlook.jp>
2023-06-04 22:04:31 +0900

    update: 茹でる部分完成。

--- src/main/java/com/example/examplemod/ramen_mod/block/BlockSundo.java
+++ src/main/java/com/example/examplemod/ramen_mod/block/BlockSundo.java
@@ -1,14 +1,22 @@
 package com.example.examplemod.ramen_mod.block;
 
+import com.example.examplemod.ExampleMod;
+import com.example.examplemod.ramen_mod.item.ItemDryNoodle;
 import com.example.examplemod.ramen_mod.item.ItemNoodle;
+import com.example.examplemod.ramen_mod.item.ItemWaterNoodle;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
 import net.minecraft.world.entity.player.Player;
@@ -17,6 +25,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -24,9 +33,11 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jline.utils.Log;
 
 import java.util.Objects;
+import java.util.Random;
 import java.util.UUID;
 
 public class BlockSundo extends Block {
@@ -55,14 +66,34 @@ public class BlockSundo extends Block {
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         boolean water = pState.getValue(WATER); // 今の色を取得
+        ItemStack heldItem = pPlayer.getItemInHand(pHand);
 
         if (pLevel.isClientSide) {
             return InteractionResult.SUCCESS;
         } else {
-            // 色を変更して設定
-            pLevel.setBlockAndUpdate(pPos, pState.setValue(WATER, !water));
+            if (heldItem.getItem() == Items.WATER_BUCKET) {
+                // 水バケツを持っている場合はブロックの状態を切り替える
+                pLevel.setBlockAndUpdate(pPos, pState.setValue(WATER, !water));
 
-            return InteractionResult.CONSUME;
+                // 空バケツに変更する
+                if (!pPlayer.isCreative()) {
+                    pPlayer.setItemInHand(pHand, new ItemStack(Items.BUCKET));
+                }
+
+                return InteractionResult.CONSUME;
+            } else if (heldItem.getItem() == Items.BUCKET) {
+                // 水バケツを持っている場合はブロックの状態を切り替える
+                pLevel.setBlockAndUpdate(pPos, pState.setValue(WATER, !water));
+
+                // 空バケツに変更する
+                if (!pPlayer.isCreative()) {
+                    pPlayer.setItemInHand(pHand, new ItemStack(Items.WATER_BUCKET));
+                }
+
+                return InteractionResult.CONSUME;
+            } else {
+                return InteractionResult.PASS;
+            }
         }
     }
 
@@ -78,13 +109,42 @@ public class BlockSundo extends Block {
             ItemStack itemStack = itemEntity.getItem();
             Item item = itemStack.getItem();
 
-            if (item instanceof ItemNoodle) {
-                // アイテムがItemNoodleである場合の処理
+            if (item instanceof ItemDryNoodle && pState.getValue(WATER)) {
+                // アイテムがItemNoodleでなおかつ、水が張っている時のみ処理をする
                 // ここに処理を追加してください
 
                 System.out.println("ItemNoodle");
 
+                // アイテムがItemDryNoodleである場合の処理
+                if (!pLevel.isClientSide) {
+                    // サーバーサイドのみで実行する
+
+                    // 30tick後に土ブロックにEntityを変えるタスクをスケジュールする
+                    pLevel.scheduleTick(pPos, pState.getBlock(), 30);
+
+                    // ここで、置かれたアイテムを消去する。
+                    itemEntity.remove(Entity.RemovalReason.DISCARDED); // アイテムを削除する
+                }
+
             }
+        }
+    }
+
+    //1.5秒ごとに呼ばれるメソッド
+    @Override
+    public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, Random pRandom) {
+        // 30tick後に実行される処理
+        if (!pLevel.isClientSide) {
+            // サーバーサイドのみで実行する
+
+            // 土ブロックのEntityを生成する
+            ItemEntity itemEntity = new ItemEntity(EntityType.ITEM, pLevel);
+            itemEntity.setItem(new ItemStack(ExampleMod.ITEM_WATER_NOODLE));
+            itemEntity.setPos(pPos.getX() + 0.5, pPos.getY() + 1.0, pPos.getZ() + 0.5);
+            pLevel.addFreshEntity(itemEntity);
+
+            // 経験値回収の音を再生する
+            pLevel.playSound(null, pPos, SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.RECORDS, 1.0f, 1.0f);
         }
     }
 }
