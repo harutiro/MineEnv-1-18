8c136a32661390cfe0340ceaddd4d6c7d4a9125c
harutiro <harutiro2027@outlook.jp>
2023-06-11 02:24:03 +0900

    update: コメントの追加

--- src/main/java/com/example/examplemod/ramen_mod/event/MouseEventHandler.java
+++ src/main/java/com/example/examplemod/ramen_mod/event/MouseEventHandler.java
@@ -17,25 +17,37 @@ public class MouseEventHandler {
 
     public static SensingBase sensing = new SensingBase();
 
+    // どうしても、マウスカーソルを動かした時に、イベントを走らせることは難しかったので、マウスホイールを回したイベントが走った時に
+    // マウスの座標を取得するようにしました。
     @SubscribeEvent
     public static void onCursorMove(InputEvent.MouseScrollEvent event) {
+        // マウスの座標を取得
         double x = event.getMouseX();
         double y = event.getMouseY();
 
         long time = System.currentTimeMillis();
 
-
+        // 現在時間、x座標、y座標を記録
         sensing.initSensorValues(x, y, time);
+        // センサー値の計算
         sensing.calculation();
+        // センサーの閾値処理　マウスを振っているかを判定
         boolean flag = sensing.checkSensor();
         if (flag) {
+            // インスタンスを取得
             Minecraft minecraft = Minecraft.getInstance();
+            // プレイヤーを取得
             LocalPlayer player = minecraft.player;
             if (player != null) {
+                // インベントリの捜査
                 for (int slot = 0; slot < player.getInventory().items.size(); slot++) {
+                    // アイテムの取得
                     ItemStack stack = player.getInventory().getItem(slot);
+                    // アイテムが水に濡れてる麺ならば
                     if (stack.getItem() == ExampleMod.ITEM_WATER_NOODLE) {
+                        // 変換するアイテムの取得
                         ItemStack convertedStack = new ItemStack(ExampleMod.ITEM_NOODLE);
+                        // インベントリのスロットに変換するアイテムをセット
                         player.getInventory().setItem(slot, convertedStack);
                         // インベントリの更新
                         player.getInventory().setChanged();
@@ -48,7 +60,5 @@ public class MouseEventHandler {
                 }
             }
         }
-
-//        System.out.println(time + "," + x + "," + y + ",");
     }
 }
